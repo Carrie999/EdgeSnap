d5b10936f1ab1101e517f7e7bffc06fee5b45ea7
carrie <771685809@qq.com>
2024-06-06 16:13:40 +0800

    Initial Commit

---
+++ EdgeSnap/EdgeSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeSnapApp.swift
+//  EdgeSnap
+//
+//  Created by  玉城 on 2024/6/6.
+//
+
+import SwiftUI
+
+@main
+struct EdgeSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
